87e07cc4d5f513ce50ceb293699bf11340a5c9bb
Aldy Hermawan <41948718+aldyhermawan1@users.noreply.github.com>
2021-09-29 20:35:15 +0800

    Spawning Enemy

---
+++ Assets/Scripts/Managers/NewEnemyManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewEnemyManager : MonoBehaviour
+{
+    public NewPlayerHealth playerHealth;
+    public GameObject enemy;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+
+    private void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn()
+    {
+        if(playerHealth.currentHealth <= 0f)
+        {
+            return;
+        }
+
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
