a3248c48b65450cbbe98e2f8fb2dd60b4fecc69c
Aldy Hermawan <41948718+aldyhermawan1@users.noreply.github.com>
2021-09-29 20:59:48 +0800

    Implement Factory Pattern

--- Assets/Scripts/Managers/NewEnemyManager.cs
+++ Assets/Scripts/Managers/NewEnemyManager.cs
@@ -7,6 +7,15 @@ public class NewEnemyManager : MonoBehaviour
     public float spawnTime = 3f;
     public Transform[] spawnPoints;
 
+    [SerializeField] EnemyFactory factory;
+    IFactory Factory
+    {
+        get
+        {
+            return factory as IFactory;
+        }
+    }
+
     private void Start()
     {
         InvokeRepeating("Spawn", spawnTime, spawnTime);
@@ -20,7 +29,9 @@ public class NewEnemyManager : MonoBehaviour
         }
 
         int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        int spawnEnemy = Random.Range(0, 3);
 
         Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        //Factory.FactoryMethod(spawnEnemy);
     }
 }
